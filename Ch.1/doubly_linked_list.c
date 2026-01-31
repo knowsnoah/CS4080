762ea9309f7c3c56fac0a3fe0bcd20dab9b7100c
noah <ojedanoah24@gmail.com>
2026-01-31 12:16:59 -0800

    Updated doubly_linked_list.c
    Add the mandatory functions of inserting, finding, and printing along with testing in the main.

--- Ch.1/doubly_linked_list.c
+++ Ch.1/doubly_linked_list.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-
 //defining Node
 typedef struct Node {
     int data;
@@ -10,17 +8,91 @@ typedef struct Node {
     struct Node *prev;
 } Node;
 
+//making head and tail a global variable
+struct Node *head = NULL;
+struct Node *tail = NULL;
+
+//function for creating a new node for insertAtHead function
+struct Node *getNewNode(int x) {
+    Node *newNode = (Node *)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        perror("malloc");
+        exit(1);
+    }
+    newNode->data = x;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+//inserting node at the head of the list
+void insertAtHead(int x) {
+    struct Node *newNode = getNewNode(x);
+    if (head == NULL) {
+        head = tail = newNode;
+        return;
+    }
+    head->prev = newNode;
+    newNode->next = head;
+    head = newNode;
+    return;
+
+}
+
+void insertAtTail(int x) {
+    struct Node *newNode = getNewNode(x);
+    if (head == NULL) {
+        head = tail = newNode;
+    }
+
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = newNode;
+    return;
+}
+
+//function to find a node
+void findNode(int x) {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
+
+    struct Node *temp = head;
+    while (temp != NULL) {
+        if (temp->data == x) {
+            printf("%d exists\n", x);
+            return;
+        }
+        temp = temp->next;
+    }
+    printf("%d does not exist\n", x);
+    return;
+}
+
+//printing function to return the list
+void Print() {
+    struct Node *temp = head;
+    printf("Forward: ");
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+    printf("\n");
+
+}
+
+
+//testing functions
 int main() {
-    //returning hello world
-    printf("Hello World!\n");
 
-    struct Node *head = NULL;
+    insertAtHead(10);
+    insertAtHead(20);
+    insertAtTail(100);
 
-    int a = 5;
-    int b = 10;
-    int *c = &a;
+    findNode(9);
 
-    printf("%d\n", c);
+    Print();
 
     return 0;
 }
